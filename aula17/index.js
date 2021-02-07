8532d46cafb673427d075344b46793bc5bdf7f48
reinaldoweb@hotmail.com <reinaldoweb@hotmail.com>
2021-02-07 17:11:16 -0300

    Adicionado aula 17 sobre funçao

---
+++ aula17/index.js
@@ -0,0 +1,33 @@
+// // function saudacao(nome){
+// // return `Bom dia ${nome}!`;
+
+// // }
+
+// // saudacao('Reinaldo');
+// // saudacao('Carla');
+// // saudacao('DAniel');
+// // const variavel = saudacao('Reinaldo');
+// // console.log(variavel);
+
+// function soma(x,y){
+//   const resultado = x + y;
+//   return resultado;
+// }
+
+// // console.log(soma(2,2));
+// // console.log(soma(2,5));
+// // console.log(soma(8,2));
+// const resultado = soma(3,3);
+// console.log(resultado);
+
+// const raiz = function(n){
+//   return n ** 0.5;
+// };
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+ const raiz = n => n ** 0.5;
+
+  console.log(raiz(9));
+  console.log(raiz(16));
+  console.log(raiz(25));
