40f9b4b21c05758df1f26e710cc312621e01edc3
reinaldoweb@hotmail.com <reinaldoweb@hotmail.com>
2021-02-09 14:28:50 -0300

    Aula de funçao e array

---
+++ aula20/js/script.js
@@ -0,0 +1,28 @@
+function meuEscopo() {
+
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
+
+  const pessoas = [];
+
+
+  function recebeEventoForm(evento) {
+    evento.preventDefault();
+
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
+
+    pessoas.push({
+      nome: nome.nodeValue,
+      sobrenome: sobrenome.nodeValue,
+      peso = peso.nodeValue,
+      altura = altura.value
+    });
+
+  }
+  form.addEventListener('submit', recebeEventoForm);
+
+}
+meuEscopo();
