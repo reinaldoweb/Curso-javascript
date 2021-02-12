f735b23df173bf2da26f52fcde631c12141ac6e4
Reinaldo <reinaldoweb@hotmail.com>
2021-02-12 14:48:17 -0300

    Avaliaçao de curto circuito

---
+++ aula23/index.js
@@ -0,0 +1,13 @@
+//Avaliação de curto-circuito
+
+/**
+ * 
+ */
+
+ const a = 0;
+ const b = null;
+ const c = 'false';// Aqui valor real verdadeiro entre aspas
+ const d = NaN;
+ const e = False;
+
+ console.log(a || b || c || d);
