bcb74dd8b62503a69382ddd9bb7ae5f013113a6a
Reinaldo <reinaldoweb@hotmail.com>
2021-02-12 14:15:46 -0300

    operadores logicos

--- aula22/index.js
+++ aula22/index.js
@@ -5,4 +5,20 @@ Operadores Lógicos
 ! -> NOT -> NÃO
 */
 
-console.log(true && true);
+// console.log(true && true);
+// const expressao = true && true && false && true; //END
+// console.log(expressao);
+
+//const expressaoAnd = true && true && false && true; //and
+// const expressaoOR= false || true; //OR
+// console.log(expressaoOR);
+
+// const usuario = 'Reinaldo';
+// const senha = '123456';
+
+// const vailogar = usuario === 'Reinaldo' && senha === '12345';
+// console.log(vailogar);
+
+// ! -> negação
+
+console.log(!false);
