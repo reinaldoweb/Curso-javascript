94c56508452fab5bee2d87acf7ca19fb44e382af
Reinaldo <reinaldoweb@hotmail.com>
2021-02-12 11:07:44 -0300

    Operadores de comparaçao

---
+++ aula21/index.js
@@ -0,0 +1,16 @@
+/*
+Operador de comparação
+> maoir que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade de valor
+=== igualdade de valor e tipo
+!= diferente (valor)
+!== diferente valor e tipo
+*/
+const num1 = 10;
+const num2 = '10';
+const comp = num1 === num2;
+console.log(comp);
+
