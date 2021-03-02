cfa90c15bb5656da4258977d2ea1bf1c9cfe9781
Reinaldo <reinaldoweb@hotmail.com>
2021-03-02 14:09:01 -0300

    Exercicio com Node-List

---
+++ aula37/assets/js/main.js
@@ -0,0 +1,13 @@
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroudColorBody = estilosBody.backgroundColor;
+console.log(backgroudColorBody);
+
+for (let p of ps) {
+
+  p.style.backgroundColor = backgroudColorBody;
+  p.style.Color = '#FFFFFF';
+  
+}
