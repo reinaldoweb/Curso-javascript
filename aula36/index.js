ca5db9410ec36986d356ab72f4be1f24ec84b294
Reinaldo <reinaldoweb@hotmail.com>
2021-03-02 13:42:03 -0300

    For Of - Extrutura de repetiçao

---
+++ aula36/index.js
@@ -0,0 +1,21 @@
+//For classico geralmente com interavies (array ou strings)
+//For IN retorna indice ou chave (String, arry ou objetos)
+//For OF retorn o valor em si(interaveis, arreis ou strings)
+// const nomes = ['Reinaldo', 'Santos', 'Jesus'];
+
+// for(let i = 0; i< nome.length; i++){
+//   console.log(nome[i]);
+// }
+
+// for(let index in nome){
+//   console.log(nome[index]);
+// }
+
+// for (let valor of nomes){
+//   console.log(valor);
+// }
+
+// nomes.forEach(function(valor, indice){
+// console.log(valor, indice);
+  
+// });
