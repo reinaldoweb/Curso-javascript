caa44a0b8bb3368b0485c69b4a8ce002c5550099
Reinaldo <reinaldoweb@hotmail.com>
2021-03-11 16:15:36 -0300

    Retorno da funçao

---
+++ aula46-retornoDaFunção/index.js
@@ -0,0 +1,15 @@
+//Retorno da função
+
+function criaMultiplicador(multiplicador){
+  return function(n){
+    return n* multiplicador;
+  };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(3));
+console.log(quadriplica(4));
