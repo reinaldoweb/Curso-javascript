46cef3e43ba49d30f7eb5efc36581dcf53981409
Reinaldo <reinaldoweb@hotmail.com>
2021-03-11 16:59:21 -0300

    Closures

---
+++ aula47-EscopoLéxico/closure.js
@@ -0,0 +1,14 @@
+//Closures
+function retornaFuncao(nome){
+  return function(){
+    return nome;
+  };
+}
+
+const funcao = retornaFuncao('Reinaldo');
+const funcao2 = retornaFuncao('Santos');
+
+console.dir(funcao);
+console.dir(funcao2);
+
+console.log(funcao(), funcao2());
