d54e60224b92972025e4ae40e31eaf88431e6513
Reinaldo <reinaldoweb@hotmail.com>
2021-04-26 16:01:52 -0300

    Express - req.params, req.query e req.body

--- node/aula8/server.js
+++ node/aula8/server.js
@@ -1,39 +1,31 @@
 const express = require('express');
-
 const app = express();
-
-app.use(
-  express.urlencoded(
-    {
-      extended: true
-    }
-  )
-);
+app.use(express.urlencoded({extended:true}));
 const port = 3000;
 
 app.get('/', (req, res) => {
 
     res.send(`
-    <form action="/" method="POST">
-    Nomes: <input type="text" name="nome">
-    <button>Enviar Formulário</button>
+    <form action="/" method="post" name="nome">
+    Nome do cliente: <input type="text" name="nome">
+    E-mail do cliente: <input type="text" name="email">
+
+    <button>Enviar</button>
     </form>
     `); 
-
 });
 
-app.get('/testes/:idUsuario?', (req, res) => {
+app.get('/testes/:idUsuarios?/:parametro?', (req,res) => {
   console.log(req.params);
-  res.send('olá');
-})
+  console.log(req.query);
+  res.send(req.params);
+});
 
 app.post('/', (req, res) => {
-  res.send("Recebi o fomulário")
+  console.log(req.body);
+  res.send(`O que você me enviou foi:${req.body.nome} -  ${req.body.email} `);
 });
 
-app.get('/', (req, res) => {
-  res.send('Obrigado por entrar em contato.');
-});
 
 app.listen(port, () => {
   console.log(`Exemplo app listando na parta http://localhost:${port}`);
